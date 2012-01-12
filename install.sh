09516e76c1a0577d04ea6c5e39127f8dfed8ddb3
RiANOl <ar3sti@gmail.com>
2012-01-12 11:46:12 +0800

    Direct clone oh-my-zsh and vim bundle instead of using git submodule.

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-git submodule update --init
-
 base_dir=`dirname $0`
 for file in `ls -A $base_dir`
 do
   [ ".git" = $file ] && continue
-  [ ".gitmodules" = $file ] && continue
   [ "install.sh" = $file ] && continue
   [ "README" = $file ] && continue
   if [ -e ~/$file ]; then
@@ -16,3 +13,9 @@ do
     cp -R "$base_dir/$file" ~/
   fi
 done
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
+git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/vim-markdown
+git clone https://github.com/Shougo/neocomplcache.git ~/.vim/bundle/neocomplcache
