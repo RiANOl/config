560615476047a3bcbab4861aaaf9b1f5d22f4595
RiANOl <ar3sti@gmail.com>
2012-01-14 03:06:02 +0800

    Add vim-coffee-script, vim-css3-syntax, vim-javascript, vim-ruby

--- install.sh
+++ install.sh
@@ -15,7 +15,11 @@ do
 done
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
-git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/vim-markdown
 git clone https://github.com/Shougo/neocomplcache.git ~/.vim/bundle/neocomplcache
+git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/hail2u/vim-css3-syntax.git ~/.vim/bundle/vim-css3-syntax
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/vim-markdown
+git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
+git clone https://github.com/vim-ruby/vim-ruby.git ~/.vim/bundle/vim-ruby
