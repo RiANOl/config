77be53fdce6803887edbbfdec33b08e118aaa459
RiANOl <ar3sti@gmail.com>
2012-03-19 13:00:37 +0800

    Fix error that cannot linking the correct directories

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-git submoudle update --init
+cd `dirname $0`
+git submodule update --init
 
-base_dir=`dirname $0`"/home"
+cd home
+base_dir=`pwd -P`
+
 for file in `ls -A $base_dir`
 do
   if [ -e ~/$file ]; then
-     mv "~/$file" "~/$file.bak"
-  fi
-  ln -s "$base_dir/$file" "~/$file"
+     mv ~/$file ~/$file.bak
+   fi
+  ln -s $base_dir/$file ~/$file
 done
