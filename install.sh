293f10eda698dadc078dc200f758c5bd2161cce7
RiANOl <ar3sti@gmail.com>
2012-03-19 12:33:13 +0800

    Use git submodule instead direct clone

--- install.sh
+++ install.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
+git submoudle update --init
+
 base_dir=`dirname $0`"/home"
 for file in `ls -A $base_dir`
 do
   if [ -e ~/$file ]; then
-     echo "$file is exist."
-     continue
-  else
-    cp -R "$base_dir/$file" ~/
+     mv "~/$file" "~/$file.bak"
   fi
+  ln -s "$base_dir/$file" "~/$file"
 done
-
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/Shougo/neocomplcache.git ~/.vim/bundle/neocomplcache
-git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
-git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-git clone https://github.com/hail2u/vim-css3-syntax.git ~/.vim/bundle/vim-css3-syntax
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/vim-markdown
-git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
-git clone https://github.com/vim-ruby/vim-ruby.git ~/.vim/bundle/vim-ruby
