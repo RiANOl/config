670ab412caa13dd3451d675d217d9c164158b1fa
Rianol Jou <rianol.jou@gmail.com>
2022-03-25 00:12:21 +0800

    Fix install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ git submodule update --init --recursive
 
 base_dir=`pwd`
 
-for file in `ls -A "${base_dir}/home"`
+for file in `ls -A ${base_dir}/home`
 do
-    if [[ ! -L "~/${file}" ]]; then
-        [[ -f "~/${file}" ]] && mv "~/${file}" "~/${file}.bak"
-        ln -s "${base_dir}/home/${file}" "~/${file}"
+    if [[ ! -L ~/${file} ]]; then
+        [[ -f ~/${file} ]] && mv ~/${file} ~/${file}.bak
+        ln -s ${base_dir}/home/${file} ~/${file}
     fi
 done
 
-ZIM_HOME='~/.zim'
+ZIM_HOME=~/.zim
 
 if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
     curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
