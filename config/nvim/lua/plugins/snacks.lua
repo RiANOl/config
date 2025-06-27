bd6a53c4dac2a6cbf3797a09b2a475779a3a26b9
Rianol Jou <rianol@never.garden>
2025-06-28 01:13:59 +0800

    chore(config/nvim): add gitui integration

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -37,6 +37,7 @@ return {
             { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
             { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
             { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+            { "<leader>gu", function() Snacks.terminal('gitui') end, desc = "GitUI" },
             -- Grep
             { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
             { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
